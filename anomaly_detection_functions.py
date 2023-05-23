553d87fca258d0ee1eeb9687613e690bd47793dd
cdgonzalezr1 <92879774+cdgonzalezr1@users.noreply.github.com>
2023-05-23 04:44:35 -0400

    Anomaly detection boosted

--- anomaly_detection_functions.py
+++ anomaly_detection_functions.py
@@ -4,8 +4,8 @@ import pandas as pd
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
-
-
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 
 
@@ -48,11 +48,12 @@ def preprocess_data(data, numerical_columns, categorical_columns):
 
 
 
-def detect_anomalies(data, cluster_col, features, contamination=0.1):
+def detect_anomalies(data, cluster_col, features, contamination=0.05):
     unique_clusters = data[cluster_col].unique()
     data['anomaly_label'] = 0
     data['original_index'] = data.index
     data = data.reset_index(drop=True)
+    data = data.dropna()
 
     scaler = MinMaxScaler(feature_range=(0, 1))
 
@@ -67,11 +68,27 @@ def detect_anomalies(data, cluster_col, features, contamination=0.1):
             anomaly_labels = isolation_forest.predict(cluster_data[features])
 
             # Invierte y reescala las puntuaciones de anomalía
-            rescaled_anomaly_scores = 1 - scaler.fit_transform(-anomaly_scores.reshape(-1, 1)).flatten()
+            rescaled_anomaly_scores = scaler.fit_transform(-anomaly_scores.reshape(-1, 1)).flatten()
 
             data.loc[cluster_data.index, 'anomaly_score'] = rescaled_anomaly_scores
 
     data = data.set_index('original_index')
+    
+    # Aplica PCA para visualizar todas las dimensiones
+    pca = PCA(n_components=3)
+    pca_data = pca.fit_transform(data[features])
+    pca_df = pd.DataFrame(data=pca_data, columns=['PCA1', 'PCA2', 'PCA3'])
+    pca_df.index = data.index
+
+    # Crea un nuevo dataframe con los valores anómalos
+    anomalies = pca_df[data['anomaly_label'] == 1]
+
+    # Grafica los datos normales en azul y los anómalos en rojo
+    fig, ax = plt.subplots(figsize=(10, 8))
+    ax.scatter(pca_df['PCA1'], pca_df['PCA2'], c='b', label='Normal')
+    ax.scatter(anomalies['PCA1'], anomalies['PCA2'], c='r', label='Anomalies')
+    ax.legend()
+    plt.show()
 
     return data
 
